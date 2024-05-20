1e5676e2d45421b66a1912027ae0c469e1ebce36
kitiya <parn.takhum@gmail.com>
2024-05-20 22:59:56 +0700

    create caed view

--- Scrumdinger/Scrumdinger/Models/CardView.swift
+++ Scrumdinger/Scrumdinger/Models/CardView.swift
@@ -14,13 +14,23 @@ struct CardView: View {
         VStack(alignment: .leading) {
                    Text(scrum.title)
                        .font(.headline)
+            Spacer()
+                       HStack {
+                           Label("\(scrum.attendees.count)", systemImage: "person.3")
+                           Spacer()
+                           Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                               .padding(.trailing, 20)
+                       }
+                       .font(.caption)
                }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
 
 struct CardView_Previews: PreviewProvider {
-    static var scrum = DailyScrum.sampleData[0]
+    static var scrum = DailyScrum.sampleData[2]
     static var previews: some View {
         CardView(scrum: scrum)
             .background(scrum.theme.mainColor)
